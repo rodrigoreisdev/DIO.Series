a132263153e2fb0b287e521c8db4b785656bfc6b
rodrigoreisdev <67196386+rodrigoreisdev@users.noreply.github.com>
2021-03-08 23:15:48 -0300

    2

--- DIO.Series/Program.cs
+++ DIO.Series/Program.cs
@@ -6,8 +6,61 @@ namespace DIO.Series
     {
         static void Main(string[] args)
         {
-            Serie objeto = new Serie();
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while(opcaoUsuario.ToUpper() !="X")
+            {
+                switch(opcaoUsuario)
+                {
+                    case "1":
+                        break;
+
+                    case "2":
+
+                        break;
+
+                    case "3":
+
+                        break;
+                    case "4":
+
+                        break;
+
+                    case "5":
+
+                        break;
+
+                    case "C":
+                        Console.Clear();
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+            Console.WriteLine("Obrigado por utilizar nossos serviços!");
+            Console.WriteLine();
             
+        }
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("DIO Séries a seu dispor!!!");
+            Console.WriteLine("Informe a opções desejada: ");
+
+            Console.WriteLine("1- Listar séries");
+            Console.WriteLine("2- Inserir nova série");
+            Console.WriteLine("3- Atualizar série");
+            Console.WriteLine("4- Excluir série");
+            Console.WriteLine("5- Visualizar série");
+            Console.WriteLine("C- Limpar Tela");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+
         }
     }
 }
